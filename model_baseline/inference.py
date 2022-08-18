b5f40e56871898a6478857f9ffd98fcd7f1ea149
AL <allanlin666666@gmail.com>
2022-08-17 19:33:26 -0700

    make inference script unique

--- model_baseline/inference.py
+++ model_baseline/inference.py
@@ -2,7 +2,9 @@
 import torch
 import numpy as np
 from tqdm import tqdm
-from data.datasetv2 import Reconstruct
+from matplotlib import pyplot as plt
+
+from data.transforms import SeparatedReconstruct
 from train_utils import load_checkpoint
 from model_baseline.build_everything import model, dataset
 
@@ -26,7 +28,7 @@ def inference(self, seq: torch.Tensor, idx=1, deterministic=False):
     return seq
 
 
-run_name = 'baseline_1.1'
+run_name = 'baseline_2.91'
 run_dir  = f'experiments/{run_name}'
 
 load_checkpoint(run_dir, model)
@@ -34,12 +36,23 @@ model = model.cuda()
 
 # %%
 x, y = dataset.get()
-x = x[0]
+plt.hist(x[0, 0], bins=91)
+plt.show()
+plt.hist(x[0, 1], bins=129)
+plt.show()
+plt.hist(x[0, 2], bins=200)
+plt.show()
+print(x[:, 0].max())
+print(np.histogram(x[0, 0], bins=91, range=(0, 91)))
 x = torch.from_numpy(x).cuda()
 y = torch.from_numpy(y).cuda()
 
 yn, yv, yd = model(x)
 print((y[:, 0] == yn.argmax(-1)).float().mean())
+print((y[:, 1] == yv.argmax(-1)).float().mean())
+print((y[:, 2] == yd.argmax(-1)).float().mean())
+# %%
+print(x[0, 1])
 
 # %%
 seq = torch.zeros([1, 3, 512], dtype=torch.int32).cuda()
@@ -52,13 +65,12 @@ seq = inference(model, seq, deterministic=False)
 seq[:, :, -1] = 1
 seq = seq[0].cpu().numpy()
 print(seq)
-rec = Reconstruct(duration_lin_bins=200)
+rec = SeparatedReconstruct(duration_lin_bins=200)
 rec.binned_encoding_to_file('gen_test1.midi', seq)
 
 # %%
-rec = Reconstruct(duration_lin_bins=200)
-rec.binned_encoding_to_file("get_test1.midi", dataset.get()[1][0])
+rec = SeparatedReconstruct(duration_lin_bins=200)
+rec.binned_encoding_to_file("get_test1.midi", dataset.get()[0][0])
 # %%
-from matplotlib import pyplot as plt
-plt.hist(seq[2], bins=100)
+plt.hist(seq[1], bins=100)
 
