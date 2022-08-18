5884db7d2a5f7717be900fd9b5ef2e4a753876f8
AL <allanlin666666@gmail.com>
2022-08-17 19:37:32 -0700

    Scaled up model_baseline

--- model_baseline/build_everything.py
+++ model_baseline/build_everything.py
@@ -20,13 +20,13 @@ m_args = EasyDict()
 m_args.vocab_note            = 89  + 3     # extra padding tokens
 m_args.vocab_velocity        = 127 + 3     # extra padding tokens
 m_args.vocab_duration        = 200 + 3     # extra padding tokens
-m_args.embed_note            = 128
-m_args.embed_velocity        = 128
-m_args.embed_duration        = 256
-m_args.n_layers              = 6
+m_args.embed_note            = 128 * 2
+m_args.embed_velocity        = 128 * 2
+m_args.embed_duration        = 256 * 2
+m_args.n_layers              = 10
 m_args.n_heads               = 8
 m_args.max_sequence          = 1024
-m_args.proj_forward          = 2048
+m_args.proj_forward          = 4096
 m_args.dropout               = 0.1 
 m_args.activation            = 'gelu'
 m_args.loss_weights          = {'loss_notes': 1, 'loss_velocity': 1, 'loss_duration': 1}
@@ -61,20 +61,22 @@ from data.transforms import SeparatedEncoding
 d_args = EasyDict()
 
 data_dir = r'datasets/maestro/maestro-v3.0.0'
-d_args.batch_size            = 4
+d_args.batch_size            = 8
 d_args.workers               = 2
 d_args.seq_len               = 1025               # one extra token since loss is auto-regressive
 d_args.duration_lin_bins     = 200                # number of linear (same width) bins/embeddings for duration
 d_args.note_shifts           = (-5, 5)            # random integer shifts in note for augmentation
 d_args.velocity_shifts       = (-7, 7)            # augment velocities as well
 d_args.duration_muls         = (0.8, 1.2)         # random multiplication to duration from uniform distribution
+d_args.clip_time_skip        = 10                 # most time skips are too small to matter, removes any under this value
 
 actors = [SeparatedEncoding.remote(
     d_args.seq_len,
     d_args.note_shifts,
     d_args.velocity_shifts,
     d_args.duration_muls,
-    d_args.duration_lin_bins
+    d_args.duration_lin_bins,
+    clip_time_skip=d_args.clip_time_skip
 ) for _ in range(d_args.workers)]
 dataset = Dataset(data_dir, d_args.batch_size, actors)
 
@@ -85,7 +87,7 @@ dataset = Dataset(data_dir, d_args.batch_size, actors)
 ###################
 if __name__ == '__main__':
     from train_utils import training_loop, Logger
-    run_name = 'baseline_2.6'
+    run_name = 'baseline_2.91'
     train_args = EasyDict()
     train_args.epochs                     = 120
     train_args.run_dir                    = f'experiments/{run_name}'
@@ -100,7 +102,7 @@ if __name__ == '__main__':
     configs.optimizer_args                = opt_args
     configs.data_args                     = d_args
     configs.batch_size                    = d_args.batch_size
-    configs.description                   = 'naive baseline model with no adjustments'
+    configs.description                   = 'weight different classes differently'
 
     run_fn = lambda: wandb.init(project='MusicGeneration', entity='allanl', dir=train_args.run_dir, 
         group=run_name, id=run_name, config=configs, resume='allow')
@@ -111,5 +113,5 @@ if __name__ == '__main__':
 
     run = run_fn()
     with run:
-        wandb.watch(model)
+        wandb.watch(model, log_freq=500)
         training_loop(model, dataset, criterion, optimizer, lr_scheduler, logger, **train_args)
